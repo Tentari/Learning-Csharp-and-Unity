d3a64ac3f446be1bb39895b744a2ee83b35ed258
Tentari <renatas911x@gmail.com>
2024-09-23 23:28:23 +0300

    New code for glad arena

--- Csharp/OOP/Deck of cards/obj/Debug/net8.0/Deck of cards.AssemblyInfo.cs
+++ Csharp/OOP/Deck of cards/obj/Debug/net8.0/Deck of cards.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Deck of cards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4122ca30243ad4f5a0847991d801446618983639")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65e22ebd7c05ec378d3dee15f285b560bf71e451")]
 [assembly: System.Reflection.AssemblyProductAttribute("Deck of cards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Deck of cards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Csharp/OOP/Glad Arena/Program.cs
@@ -0,0 +1,403 @@
+﻿namespace Glad_Arena;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Arena arena = new Arena();
+
+        arena.Work();
+
+        Console.ReadKey();
+    }
+}
+
+public class Arena
+{
+    private List<Gladiator> _allGladiators;
+    private Gladiator _firstGladiator;
+    private Gladiator _secondGladiator;
+
+    public Arena()
+    {
+        _allGladiators = new List<Gladiator> { new Fighter(), new Tyyr(), new Tank(), new Mage(), new Dagger() };
+        ChooseGladiators();
+    }
+
+    public void Work()
+    {
+        Console.WriteLine("Hello player. Choose your gladiators!");
+
+        Console.Clear();
+        Fight();
+
+
+        WinnerMessage();
+    }
+
+    private void Fight()
+    {
+        int damageDoneFirst;
+        int damageDoneSecond;
+
+        while (_firstGladiator.Health > 0 && _secondGladiator.Health > 0)
+        {
+            _firstGladiator.Attack(_secondGladiator, out damageDoneFirst);
+
+            _secondGladiator.Attack(_firstGladiator, out damageDoneSecond);
+
+            Console.WriteLine(
+                $"{_firstGladiator.Name} attacks {_secondGladiator.Name} for {damageDoneFirst} DMG");
+
+            Console.WriteLine(
+                $"{_secondGladiator.Name} attacks {_firstGladiator.Name} for {damageDoneSecond} DMG");
+
+            Console.WriteLine(
+                $"First gladiator({_firstGladiator.Name}) HP:{_firstGladiator.Health} Second gladiator({_secondGladiator.Name}) HP:{_secondGladiator.Health}\n");
+        }
+    }
+
+    private void WinnerMessage()
+    {
+        if (_firstGladiator.Health <= 0 && _secondGladiator.Health <= 0)
+        {
+            Console.WriteLine("Draw");
+        }
+        else if (_firstGladiator.Health > 0)
+        {
+            Console.WriteLine($"first gladiator({_firstGladiator.Name}) wins");
+        }
+        else if (_secondGladiator.Health > 0)
+        {
+            Console.WriteLine($"second gladiator({_secondGladiator.Name}) wins");
+        }
+    }
+
+    private void ChooseGladiators()
+    {
+        ShowGladiators();
+
+        Console.WriteLine("Choose first gladiator: ");
+        _firstGladiator = CreateGladiator();
+
+        ShowGladiators();
+
+        Console.WriteLine("Choose second gladiator: ");
+        _secondGladiator = CreateGladiator();
+    }
+
+    private Gladiator CreateGladiator()
+    {
+        Gladiator gladiator = null;
+
+        bool isCreated = false;
+
+        int index = ConsoleUtils.ReadInt() - 1;
+
+        while (isCreated == false)
+        {
+            if (index < _allGladiators.Count && index >= 0)
+            {
+                isCreated = true;
+                gladiator = _allGladiators[index].Clone();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        return gladiator;
+    }
+
+    private void ShowGladiators()
+    {
+        int gladiatorsNumber = 1;
+
+        Console.WriteLine();
+
+        foreach (Gladiator gladiator in _allGladiators)
+        {
+            Console.Write(gladiatorsNumber++ + ". ");
+            gladiator.ShowInfo();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+public abstract class Gladiator
+{
+    public Gladiator(int health, int damage, string name)
+    {
+        Name = name;
+        Health = health;
+        Damage = damage;
+    }
+
+    public string Name { get; private set; }
+    public int Health { get; protected set; }
+    public int Damage { get; protected set; }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"{Name} - {Health} HP / {Damage} DMG");
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        if (damage > 0)
+        {
+            Health -= damage;
+        }
+    }
+
+    public virtual void Attack(Gladiator gladiator, out int damageDone)
+    {
+        damageDone = Damage;
+
+        gladiator.TakeDamage(Damage);
+    }
+
+    public abstract Gladiator Clone();
+}
+
+public class Fighter : Gladiator
+{
+    private static Random s_random = new Random();
+
+    public Fighter(int health = 1000, int damage = 50, string name = "Fighter") : base(health, damage, name)
+    {
+    }
+
+    public override void Attack(Gladiator gladiator, out int damageDone)
+    {
+        damageDone = Damage;
+
+        int tempDamage = Damage;
+
+        if (IsCritical())
+        {
+            Console.WriteLine("Critical hit!");
+
+            Damage *= 2;
+
+            base.Attack(gladiator, out damageDone);
+        }
+        else
+        {
+            base.Attack(gladiator, out damageDone);
+        }
+
+        Damage = tempDamage;
+    }
+
+    private bool IsCritical()
+    {
+        int random = s_random.Next(0, 100);
+        int minNumberToCrit = 70;
+
+        if (random >= minNumberToCrit)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override Gladiator Clone()
+    {
+        return new Fighter();
+    }
+}
+
+public class Tyyr : Gladiator
+{
+    private int _attackCount;
+    private int _maxAttackCount;
+
+    public Tyyr(int health = 500, int damage = 200, string name = "Tyyr") : base(health, damage, name)
+    {
+        _attackCount = 0;
+        _maxAttackCount = 3;
+    }
+
+    public override void Attack(Gladiator gladiator, out int damageDone)
+    {
+        damageDone = Damage;
+
+        base.Attack(gladiator, out damageDone);
+
+        if (_attackCount >= _maxAttackCount)
+        {
+            base.Attack(gladiator, out damageDone);
+
+            Console.WriteLine($"Crit! Double damage!");
+
+            damageDone = Damage * 2;
+
+            _attackCount = 0;
+        }
+        else
+        {
+            _attackCount++;
+        }
+    }
+
+    public override Gladiator Clone()
+    {
+        return new Tyyr();
+    }
+}
+
+public class Tank : Gladiator
+{
+    private int _maxHealth;
+    private int _rage;
+
+    public Tank(int health = 3000, int damage = 10, string name = "Tank") : base(health, damage, name)
+    {
+        _rage = 0;
+        _maxHealth = health;
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+
+        int maxRage = 100;
+        int rageGain = 20;
+
+        if (_rage >= maxRage)
+        {
+            CastHeal();
+            _rage = 0;
+        }
+        else
+        {
+            _rage += rageGain;
+        }
+    }
+
+    public void CastHeal()
+    {
+        int healAmmount = 500;
+
+        if (_maxHealth > Health + healAmmount)
+        {
+            Health += healAmmount;
+
+            Console.WriteLine("Blessed heal!");
+        }
+        else
+        {
+            Health = _maxHealth;
+
+            Console.WriteLine("Healed to full.");
+        }
+    }
+
+    public override Gladiator Clone()
+    {
+        return new Tank();
+    }
+}
+
+public class Mage : Gladiator
+{
+    private int _mana;
+
+    public Mage(int health = 1000, int damage = 5, string name = "Mage") : base(health, damage, name)
+    {
+        _mana = 100;
+    }
+
+    public override void Attack(Gladiator gladiator, out int damageDone)
+    {
+        int tempDamage = Damage;
+
+        if (_mana >= 20)
+        {
+            _mana -= 20;
+
+            CastFireball();
+
+            base.Attack(gladiator, out damageDone);
+
+            Damage = tempDamage;
+        }
+        else
+        {
+            base.Attack(gladiator, out damageDone);
+
+            Console.WriteLine("No mana, attacking with hand.");
+        }
+    }
+
+    public void CastFireball()
+    {
+        Console.WriteLine("Fireball casted!");
+
+        Damage += 300;
+    }
+
+    public override Gladiator Clone()
+    {
+        return new Mage();
+    }
+}
+
+public class Dagger : Gladiator
+{
+    private static Random s_random = new Random();
+
+    public Dagger(int health = 1000, int damage = 100, string name = "Dagger") : base(health, damage, name)
+    {
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        if (IsDodged() == false)
+        {
+            base.TakeDamage(damage);
+        }
+        else
+        {
+            Console.WriteLine("Dodged!");
+            base.TakeDamage(0);
+        }
+    }
+
+    private bool IsDodged()
+    {
+        int random = s_random.Next(0, 100);
+        int minNumberToDodge = 65;
+
+        if (random >= minNumberToDodge)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override Gladiator Clone()
+    {
+        return new Dagger();
+    }
+}
+
+public class ConsoleUtils
+{
+    public static int ReadInt()
+    {
+        int number;
+
+        while (int.TryParse(Console.ReadLine(), out number) == false)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+
+        return number;
+    }
+}
