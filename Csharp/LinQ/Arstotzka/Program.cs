cbd8001e4da6e2036b84aaba6663f98cf378c5c1
Tentari <41296534+Tentari@users.noreply.github.com>
2024-09-30 22:42:36 +0300

    Updatee

--- Csharp/LinQ/Arstotzka/Program.cs
+++ Csharp/LinQ/Arstotzka/Program.cs
@@ -6,137 +6,75 @@ class Program
     {
         DataBaseFactory dataBaseFactory = new DataBaseFactory();
 
-        PoliceStation policeStation = dataBaseFactory.Create();
+        Arstotzka arstotzka = dataBaseFactory.Create();
 
-        policeStation.Work();
+        arstotzka.Work();
     }
 }
 
 public class DataBaseFactory
 {
-    private WantedFactory _wantedFactory;
+    private CriminalFactory _criminalFactory;
 
     public DataBaseFactory()
     {
-        _wantedFactory = new WantedFactory();
+        _criminalFactory = new CriminalFactory();
     }
 
-    public PoliceStation Create()
+    public Arstotzka Create()
     {
-        return new PoliceStation(Fill());
+        return new Arstotzka(Fill());
     }
 
-    public List<Wanted> Fill()
+    public List<Criminal> Fill()
     {
-        int wantedCount = 15;
+        int criminalsCount = 15;
 
-        List<Wanted> wanteds = new List<Wanted>();
+        List<Criminal> criminals = new List<Criminal>();
 
-        for (int i = 0; i < wantedCount; i++)
+        for (int i = 0; i < criminalsCount; i++)
         {
-            wanteds.Add(_wantedFactory.Create());
+            criminals.Add(_criminalFactory.Create());
         }
 
-        return wanteds;
+        return criminals;
     }
 }
 
-public class PoliceStation
+public class Arstotzka
 {
-    private List<Wanted> _wanted;
+    private List<Criminal> _criminals;
 
-    public PoliceStation(List<Wanted> wanted)
+    public Arstotzka(List<Criminal> criminals)
     {
-        _wanted = wanted;
+        _criminals = criminals;
     }
 
     public void Work()
     {
-        const string ShowByNationalityCommand = "1";
-        const string ShowByHeightCommand = "2";
-        const string ShowByWeightCommand = "3";
-        const string ExitCommand = "4";
+        ShowCriminals(_criminals);
 
-        bool isOpen = true;
+        _criminals = FreeCriminals("Revolution");
 
-        while (isOpen)
+        Console.WriteLine("Updated list:");
+
+        ShowCriminals(_criminals);
+    }
+
+    private List<Criminal> FreeCriminals(string crime)
+    {
+        return _criminals
+            .Where(criminals => criminals.Crime != crime)
+            .ToList();
+    }
+
+    private void ShowCriminals(List<Criminal> criminals)
+    {
+        if (criminals.Count() > 0)
         {
-            Console.WriteLine(
-                $"Welcome to our program. What do you want to do?\n{ShowByNationalityCommand} - Show wanted by nationality" +
-                $"\n{ShowByHeightCommand} - Show wanted by height" +
-                $"\n{ShowByWeightCommand} - Show wanted by weight\n{ExitCommand} - Exit");
-            string userInput = Console.ReadLine();
-
-            Console.Clear();
-
-            switch (userInput)
+            foreach (Criminal criminal in criminals)
             {
-                case ShowByNationalityCommand:
-                    ShowByNationality();
-                    break;
-
-                case ShowByHeightCommand:
-                    ShowByHeight();
-                    break;
-
-                case ShowByWeightCommand:
-                    ShowByWeight();
-                    break;
-
-                case ExitCommand:
-                    isOpen = false;
-                    break;
-
-                default:
-                    Console.WriteLine("Wrong command");
-                    break;
-            }
-        }
-    }
-
-    private void ShowByNationality()
-    {
-        Console.WriteLine("Enter nationality: ");
-        string nationality = Console.ReadLine();
-
-        IEnumerable<Wanted> filteredWanted = from wanted in _wanted
-            where wanted.Nationality.Equals(nationality, StringComparison.OrdinalIgnoreCase)
-            select wanted;
-
-        ShowWanted(filteredWanted);
-    }
-
-    private void ShowByHeight()
-    {
-        Console.WriteLine("Enter height: ");
-        int height = ConsoleUtils.ReadInt();
-
-        IEnumerable<Wanted> filteredWanted = from wanted in _wanted
-            where wanted.Height == height
-            select wanted;
-
-        ShowWanted(filteredWanted);
-    }
-
-    private void ShowByWeight()
-    {
-        Console.WriteLine("Enter weight: ");
-        int weight = ConsoleUtils.ReadInt();
-
-        IEnumerable<Wanted> filteredWanted = from wanted in _wanted
-            where wanted.Weight == weight
-            select wanted;
-
-        ShowWanted(filteredWanted);
-    }
-
-    private void ShowWanted(IEnumerable<Wanted> filteredWanted)
-    {
-        if (filteredWanted.Count() > 0)
-        {
-            foreach (Wanted wanted in filteredWanted)
-            {
-                wanted.ShowInfo();
+                criminal.ShowInfo();
             }
         }
         else
@@ -146,12 +84,11 @@ public class PoliceStation
     }
 }
 
-public class WantedFactory
+public class CriminalFactory
 {
-    public Wanted Create()
+    public Criminal Create()
     {
-        return new Wanted(GetRandomFullName(), GetRandomNationality(), GetRandomHeight(), GetRandomWeight(),
-            IsJailed());
+        return new Criminal(GetRandomFullName(), GetRandomCrime());
     }
 
     private string GetRandomFullName()
@@ -177,66 +114,33 @@ public class WantedFactory
         return fullNames[ConsoleUtils.GetRandomNumber(fullNames.Count - 1)];
     }
 
-    private string GetRandomNationality()
+    private string GetRandomCrime()
     {
-        List<string> nationalities = new List<string>()
+        List<string> crimes = new List<string>()
         {
-            "American", "British", "Canadian", "Australian", "Japanese", "Chinese", "Indian",
+            "Assault", "Robbery", "Murder", "Homicide", "Revolution"
         };
 
-        return nationalities[ConsoleUtils.GetRandomNumber(nationalities.Count - 1)];
-    }
-
-    private int GetRandomHeight()
-    {
-        List<int> heights = new List<int>()
-        {
-            160, 170, 180, 190, 200, 210
-        };
-
-        return heights[ConsoleUtils.GetRandomNumber(heights.Count - 1)];
-    }
-
-    private int GetRandomWeight()
-    {
-        List<int> weights = new List<int>()
-        {
-            70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200
-        };
-
-        return weights[ConsoleUtils.GetRandomNumber(weights.Count - 1)];
-    }
-
-    private bool IsJailed()
-    {
-        int boolTrue = 1;
-
-        return Convert.ToBoolean(ConsoleUtils.GetRandomNumber(boolTrue + 1));
+        return crimes[ConsoleUtils.GetRandomNumber(crimes.Count)];
     }
 }
 
-public class Wanted
+public class Criminal
 {
-    public Wanted(string fullName, string nationality, int height, int weight, bool isJailed)
+    private string _fullName;
+
+
+    public Criminal(string fullName, string crime)
     {
-        FullName = fullName;
-        Nationality = nationality;
-        Height = height;
-        Weight = weight;
-        IsJailed = isJailed;
+        _fullName = fullName;
+        Crime = crime;
     }
 
-    public string FullName { get; }
-    public string Nationality { get; }
-
-    public int Height { get; }
-    public int Weight { get; }
-
-    public bool IsJailed { get; }
+    public string Crime { get; private set; }
 
     public void ShowInfo()
     {
-        Console.WriteLine($"{FullName}) {Nationality}. {Height} height, {Weight} weight.");
+        Console.WriteLine($"{_fullName}) Crime - {Crime}.");
     }
 }
 
@@ -250,7 +154,7 @@ public class ConsoleUtils
 
         return random;
     }
-    
+
     public static int ReadInt()
     {
         int number;
