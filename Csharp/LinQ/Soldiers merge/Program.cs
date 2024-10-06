7c814036f5aef36ba718bcb52950a67ed5022b55
Tentari <41296534+Tentari@users.noreply.github.com>
2024-10-06 23:03:23 +0300

    Update

---
+++ Csharp/LinQ/Soldiers merge/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Soldiers_merge;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> soldiers =
+        [
+            "John",
+            "Jane",
+            "Joe",
+            "Jill",
+            "Jim",
+            "Bomba",
+        ];
+        
+        List<string> betterSoldier =
+        [
+            "Amogus",
+        ];
+
+        betterSoldier = betterSoldier.Concat(soldiers.Where(soldiers => soldiers.StartsWith("B"))).ToList();
+        
+        soldiers = soldiers.Where(soldiers => soldiers.StartsWith("B") == false).ToList();
+        
+        betterSoldier.ForEach(soldier => Console.WriteLine(soldier));
+    }
+}
