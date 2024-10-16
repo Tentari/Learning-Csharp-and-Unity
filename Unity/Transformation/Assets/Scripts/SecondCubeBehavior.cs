3402023950dea64953ae144c6ac89284e10afe69
Tentari <41296534+Tentari@users.noreply.github.com>
2024-10-16 05:59:55 +0300

    Add scripts to objects

---
+++ Unity/Transformation/Assets/Scripts/SecondCubeBehavior.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class SecondCubeBehavior : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private float _timer;
+    [SerializeField] private float _resizeAmount;
+
+    private float _originalTimer;
+
+    private void Start()
+    {
+        _originalTimer = _timer;
+    }
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+        Resize();
+        TickTimer();
+        ChangeDirectionOnTimer();
+    }
+
+    private void TickTimer()
+    {
+        _timer -= Time.deltaTime;
+    }
+
+    private void ChangeDirectionOnTimer()
+    {
+        if (_timer <= 0)
+        {
+            _resizeAmount *= -1;
+            _timer = _originalTimer;
+        }
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * (_moveSpeed * Time.deltaTime));
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(Vector3.up, _rotateSpeed * Time.deltaTime);
+    }
+
+    private void Resize()
+    {
+        transform.localScale += Vector3.one * _resizeAmount;
+    }
+}
